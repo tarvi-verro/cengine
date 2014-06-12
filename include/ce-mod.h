8beeb8ca6a5c22d03002e057467e289c6666f0ee
Tarvi Verro <tarvi.verro@gmail.com>
2014-06-12 19:09:18 +0300

    Module system header updates and introduction.
    A detailed description of what the module system does, or rather is
    supposed to do.

--- include/ce-mod.h
+++ include/ce-mod.h
@@ -1,66 +1,26 @@
 #ifndef _CE_MOD_H
-#define _CE_MOD_H 0, 2, 8
+#define _CE_MOD_H 0,2,13
 
 /**
- * DOC: api for modules to handle changes in dependencies
- * DOC:	Example changes:
- *	scene is switched
+ * DOC: ce-mod.h
+ * The functions in this header are defined to give modules a safe way to
+ * avoid functionality conflicts, initialize and manage the lifetime of
+ * modules or resources and handle dynamic loading of modules.
  *
- *	scenebuilder is unloaded/loaded
+ * For an indepth introduction, see Documentation/ce-mod-intro.txt.
  *
- *	streamer's files are dropped as scene is switched, though some might
- *	stay
- *
- *	input methods are switched
- * DOC:	Constants
- *	menu mod (ESC settings and what have you)
- *
- *	console
- * DOC: How should changes happen? 0.2.08
- * .def = "glfw-ce:2.7.8 | glfw-less:2.7.8; ge-window; ce-input; ce-loop"
- * .comment = "wrapper to provide cengine functionality"
- * .use = "&glfw-sth" // to include above mod to be initialized only after
- *		given mod
- *
- * .def = "glfw{2.7.8} | glfw:2.7.8; glfw-less:2.7.8"
- * .comment = "glfw functionality, this doesn't include "
- *
- * .def = "ce-main 0:2.8 | ce-main 0:2.8; ce-main-ctrl$; ce-main-reload$;"
- * .use = "ce-main-ctrl" * cant be '-' sep. when def missing *
- *
- * .def = "ce-frame-loop | ce-main-ctrl=frame-loop"
- *
- * .def = "glx-window | ce-window"
- * .use = "ce-input;"
- * .comment = "uses the glx api to provide a window"
- *
- * .def = "ge-effect 0:2 | OpenGL 3:3; ge-effect[] 0:2; ge-effect+sphere"
- *
- * .def = "ge-effect+particle-fire"
- * .use = "OpenGL 3:3; ge-effect 0:2"
- *
- * .def = "ce-scene;"
- * .use = "ge-effect+particle-{sparks,fire,smoke}; ce-main-ctrl=frame-loop"
- *
- * .def = "alsa-sound | ce-sound"
- *	ce_mod_rq()
- *	ce_mod_rm()
- *
- *	ce_mod_rq("ce-effect+{sparks,sun,smoke,airdistortion};ce-window")
- *
- * It is only required to specify the outermost extension.
  */
+
 /**
  * struct ce_mod - a module providing functionality to scenes
  * @comment:	some words describing your module
  * @def:	following format "[mod name]|[implements 1],[implements 2]..."
- * @use:	modules that must be loaded before given mod's @load can be
- *		called; additionally prepend the implementation with: '!' -
- *		incompatible, '#' - load by the end of init process (by
- *		loads it after everything else) - you can use this to
- *		push time-consuming initialisations towards the end, so if one
- *		fails, the init fails in less time, '&' - load immediately
- *		after current mod
+ * @use:	specify interfaces that the module needs, so associated
+ *		module will be loaded; sadditionally prepend the functionality
+ *		name with: '!' - incompatible, '#' - not immediately needed,
+ *		use this to leave time-consuming initialisations towards the
+ *		end, so if one fails, the init fails in less time, '&' - load
+ *		only (immediately) after current mod
  * @load:	function that initializes the module or %NULL if given module
  *		doesn't require initialisation; the function should return
  *		negative if the module initialisation failed
@@ -71,7 +31,7 @@
  * If multiple modules implement the same functionality, only one of them
  * will be allowed to be loaded at a given time.
  *
- * Use fcn multiple flags:
+ * Use Interface Flag '#&' Combination:
  * Using a functionality with flags '#&' will allow the fcn to only load
  * after the current mod, and also pushes it to the end.
  */
@@ -96,7 +56,7 @@ struct ce_mod {
  * functionality, and call ce_mod_use() in that main() function to start
  * initialisation process. After the initialisation for all involved mods is
  * finished, this function returns and the main should call the specified
- * ce_main_ctrl function(provided by 'ce-main-ctrl').
+ * ce_main_ctrl function (provided by 'ce-main-ctrl').
  *
  * Each time this function is called with @mod_id of the 'ce-main'
  * functionality, all the previously required mods will be dropped.
@@ -105,6 +65,8 @@ struct ce_mod {
  * Return:	negative value on failure
  */
 int ce_mod_use(int mod_id, const char* use);
+
+int ce_mod_def(int mod_id, const char* def_impl);
 
 /**
  * ce_mod_add() - registers a module
