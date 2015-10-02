873f2911c5f3c74fd60db284ec4c712038f93c47
Tarvi Verro <tarvi.verro@gmail.com>
2015-10-02 23:57:44 +0300

    Revamped build system.
    New build system hides hideous compiler commands, unless the environment
    variable PRINT_PRETTY is set so something else than 1.
    
    It's now possible to set the directory in which compiling output files are put
    via the environment variable O=build (default).

--- glx/sub.mk
+++ glx/sub.mk
@@ -4,7 +4,7 @@ SRC += glx/context.c
 SRC += glx/input.c
 # -lX11	XCreateWindow, XOpenDisplay, XMapWindow
 # -lGL	glXCreateContext, glXMakeCurrent
-LDFLAGS += -lX11 -lGL -lxcb -pthread -lX11-xcb -lxcb-keysyms -lxcb-xinput -lxcb-image
+LDFLAGS += -lX11 -lGL -lxcb -lpthread -lX11-xcb -lxcb-keysyms -lxcb-xinput -lxcb-image
 CFLAGS += -pthread
 
 
